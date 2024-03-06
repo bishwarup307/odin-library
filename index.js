dece9c4b4fba82ca62dcb68d7a3c1379be08b5b7
bishwarup307 <15094759+bishwarup307@users.noreply.github.com>
2024-03-07 00:59:20 +0530

    Add read/favorite button functionality

--- index.js
+++ index.js
@@ -19,217 +19,337 @@ const displayGrid = document.querySelector(".content-wrapper");
 // let bookList;
 
 const getLibrary = function () {
-    return JSON.parse(localStorage.getItem("library") || "[]");
+  let library;
+  const bookList = JSON.parse(localStorage.getItem("library"));
+
+  if (bookList) {
+    library = bookList.map(
+      (bookPlainObject) =>
+        new Book(
+          bookPlainObject.title,
+          bookPlainObject.author,
+          bookPlainObject.description,
+          bookPlainObject.pages,
+          bookPlainObject.genre,
+          bookPlainObject.cover,
+          bookPlainObject.rating,
+          bookPlainObject.favorite,
+          bookPlainObject.isRead
+        )
+    );
+  } else library = [];
+  return library;
+};
+
+const setLibrary = function (bookList, updateDisplay = true) {
+  localStorage.setItem("library", JSON.stringify(bookList));
+  if (updateDisplay) displayBooks();
 };
 
 const GENRES = [
-    "Fantasy",
-    "Fiction",
-    "Science Fiction",
-    "Dystopian",
-    "Action & Adventure",
-    "Mystery",
-    "Horror",
-    "Thriller & Suspense",
-    "Historical Fiction",
-    "Romance",
-    "Women’s Fiction",
-    "LGBTQ+",
-    "Contemporary Fiction",
-    "Literary Fiction",
-    "Magical Realism",
-    "Graphic Novel",
-    "Short Story",
-    "Young Adult",
-    "New Adult",
-    "Children’s",
-    "Memoir & Autobiography",
-    "Biography",
-    "Food & Drink",
-    "Art & Photography",
-    "Self-help",
-    "History",
-    "Travel",
-    "True Crime",
-    "Humor",
-    "Essays",
-    "Guide / How-to",
-    "Religion & Spirituality",
-    "Humanities & Social Sciences",
-    "Parenting & Families",
-    "Science & Technology",
-    "Children’s",
+  "Fantasy",
+  "Fiction",
+  "Science Fiction",
+  "Dystopian",
+  "Action & Adventure",
+  "Mystery",
+  "Horror",
+  "Thriller & Suspense",
+  "Historical Fiction",
+  "Romance",
+  "Women’s Fiction",
+  "LGBTQ+",
+  "Contemporary Fiction",
+  "Literary Fiction",
+  "Magical Realism",
+  "Novel",
+  "Short Story",
+  "Young Adult",
+  "New Adult",
+  "Children’s",
+  "Memoir & Autobiography",
+  "Biography",
+  "Food & Drink",
+  "Art & Photography",
+  "Self-help",
+  "History",
+  "Travel",
+  "True Crime",
+  "Humor",
+  "Essays",
+  "Guide / How-to",
+  "Religion & Spirituality",
+  "Humanities & Social Sciences",
+  "Parenting & Families",
+  "Science & Technology",
+  "Children’s",
 ];
 
+const HEART_OUTLINE =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon icon-favorite"><title>heart-outline</title><path d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z" /></svg>';
+
+const HEART_FILLED =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon icon-favorite icon-favorite-checked"><title>heart</title><path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z" /></svg>';
+
+const BOOK_READING =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon icon-read"><title>book-open-page-variant</title><path d="M19 2L14 6.5V17.5L19 13V2M6.5 5C4.55 5 2.45 5.4 1 6.5V21.16C1 21.41 1.25 21.66 1.5 21.66C1.6 21.66 1.65 21.59 1.75 21.59C3.1 20.94 5.05 20.5 6.5 20.5C8.45 20.5 10.55 20.9 12 22C13.35 21.15 15.8 20.5 17.5 20.5C19.15 20.5 20.85 20.81 22.25 21.56C22.35 21.61 22.4 21.59 22.5 21.59C22.75 21.59 23 21.34 23 21.09V6.5C22.4 6.05 21.75 5.75 21 5.5V19C19.9 18.65 18.7 18.5 17.5 18.5C15.8 18.5 13.35 19.15 12 20V6.5C10.55 5.4 8.45 5 6.5 5Z" /></svg>';
+
+const BOOK_FINISHED =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103.19 122.88" class="icon icon-read"><path d="M17.16 0h82.72a3.32 3.32 0 013.31 3.31v92.32c-.15 2.58-3.48 2.64-7.08 2.48H15.94c-4.98 0-9.05 4.07-9.05 9.05s4.07 9.05 9.05 9.05h80.17v-9.63h7.08v12.24c0 2.23-1.82 4.05-4.05 4.05H16.29C7.33 122.88 0 115.55 0 106.59V17.16C0 7.72 7.72 0 17.16 0zm3.19 13.4h2.86c1.46 0 2.66.97 2.66 2.15v67.47c0 1.18-1.2 2.15-2.66 2.15h-2.86c-1.46 0-2.66-.97-2.66-2.15V15.55c.01-1.19 1.2-2.15 2.66-2.15z" fill-rule="evenodd" clip-rule="evenodd"/></svg>';
+
+// borrowed from https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+String.prototype.hashCode = function () {
+  var hash = 0,
+    i,
+    chr;
+  if (this.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr = this.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
+const removeChildren = function (parentNode) {
+  while (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
+};
+
 class Book {
-    constructor(
-        title,
-        author,
-        description,
-        pages,
-        genre,
-        cover,
-        rating,
-        favorite = false,
-        isRead = false
-    ) {
-        this.title = title;
-        this.author = author;
-        this.description = description;
-        this.pages = pages;
-        this.genre = genre;
-        this.cover = cover;
-        this.rating = rating;
-        this.isRead = isRead;
-        this.favorite = favorite;
-        this.dateTimeAdded = new Date();
-    }
+  constructor(
+    title,
+    author,
+    description,
+    pages,
+    genre,
+    cover,
+    rating,
+    favorite = false,
+    isRead = false
+  ) {
+    this.title = title;
+    this.author = author;
+    this.description = description;
+    this.pages = pages;
+    this.genre = genre;
+    this.cover = cover;
+    this.rating = rating;
+    this.isRead = isRead;
+    this.favorite = favorite;
+    this.dateTimeAdded = new Date();
+  }
 
-    get descString() {
-        return this.title + this.author + this.genre;
-    }
+  get descString() {
+    return this.title + this.author + this.genre;
+  }
 
-    get dateAdded() {
-        return (
-            (this.dateTimeAdded.getDate() < 10 ? "0" : "") +
-            this.dateTimeAdded.getDate() +
-            "/" +
-            (this.dateTimeAdded.getMonth() + 1 < 10 ? "0" : "") +
-            (this.dateTimeAdded.getMonth() + 1) +
-            "/" +
-            this.dateTimeAdded.getFullYear()
-        );
-    }
+  get dateAdded() {
+    const dateTime = this.dateTimeAdded || JSON.parse(this.dateTimeAdded);
+    console.log(dateTime);
+    return (
+      (dateTime.getDate() < 10 ? "0" : "") +
+      dateTime.getDate() +
+      "/" +
+      (dateTime.getMonth() + 1 < 10 ? "0" : "") +
+      (dateTime.getMonth() + 1) +
+      "/" +
+      dateTime.getFullYear()
+    );
+  }
+
+  get hash() {
+    return this.title.toLowerCase().hashCode();
+  }
 }
 
 const displayBook = function (book) {
-    const bookContainer = document.createElement("div");
-    bookContainer.classList.add("box");
-    bookContainer.classList.add("flex-col");
-    bookContainer.classList.add("gap1");
+  const bookContainer = document.createElement("div");
+  bookContainer.classList.add("box");
+  bookContainer.classList.add("flex-col");
+  bookContainer.classList.add("gap1");
 
-    const imageDiv = document.createElement("div");
-    imageDiv.classList.add("book-cover");
-    imageDiv.classList.add("relative");
-    const coverImage = document.createElement("img");
-    coverImage.src = book.cover;
-    imageDiv.appendChild(coverImage);
+  const imageDiv = document.createElement("div");
+  imageDiv.classList.add("book-cover");
+  imageDiv.classList.add("relative");
+  if (book.isRead) imageDiv.classList.add("completed");
+  imageDiv.id = book.hash;
 
-    const iconDiv = document.createElement("div");
-    iconDiv.classList.add("action-icons");
-    iconDiv.classList.add("flex");
-    iconDiv.classList.add("absolute");
+  const coverImage = document.createElement("img");
+  coverImage.src = book.cover;
+  imageDiv.appendChild(coverImage);
 
-    const readIcon = document.createElement("div");
-    readIcon.innerHTML =
-        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon icon-read"><title>eye-off</title><path d="M11.83,9L15,12.16C15,12.11 15,12.05 15,12A3,3 0 0,0 12,9C11.94,9 11.89,9 11.83,9M7.53,9.8L9.08,11.35C9.03,11.56 9,11.77 9,12A3,3 0 0,0 12,15C12.22,15 12.44,14.97 12.65,14.92L14.2,16.47C13.53,16.8 12.79,17 12,17A5,5 0 0,1 7,12C7,11.21 7.2,10.47 7.53,9.8M2,4.27L4.28,6.55L4.73,7C3.08,8.3 1.78,10 1,12C2.73,16.39 7,19.5 12,19.5C13.55,19.5 15.03,19.2 16.38,18.66L16.81,19.08L19.73,22L21,20.73L3.27,3M12,7A5,5 0 0,1 17,12C17,12.64 16.87,13.26 16.64,13.82L19.57,16.75C21.07,15.5 22.27,13.86 23,12C21.27,7.61 17,4.5 12,4.5C10.6,4.5 9.26,4.75 8,5.2L10.17,7.35C10.74,7.13 11.35,7 12,7Z" /></svg>';
-    iconDiv.appendChild(readIcon);
+  const iconDiv = document.createElement("div");
+  iconDiv.classList.add("action-icons");
+  iconDiv.classList.add("flex");
+  iconDiv.classList.add("absolute");
 
-    const favoriteIcon = document.createElement("div");
-    favoriteIcon.innerHTML =
-        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon icon-favorite"><title>heart-outline</title><path d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z" /></svg>';
-    iconDiv.appendChild(favoriteIcon);
+  const readIcon = document.createElement("div");
+  readIcon.classList.add("action-icon");
+  readIcon.dataset.action = "read";
+  readIcon.innerHTML = book.isRead ? BOOK_FINISHED : BOOK_READING;
+  iconDiv.appendChild(readIcon);
 
-    const removeIcon = document.createElement("div");
-    removeIcon.innerHTML =
-        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon icon-remove"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>';
-    iconDiv.appendChild(removeIcon);
+  const favoriteIcon = document.createElement("div");
+  favoriteIcon.classList.add("action-icon");
+  favoriteIcon.dataset.action = "favorite";
+  favoriteIcon.innerHTML = book.favorite ? HEART_FILLED : HEART_OUTLINE;
+  iconDiv.appendChild(favoriteIcon);
 
-    imageDiv.appendChild(iconDiv);
-    bookContainer.appendChild(imageDiv);
+  const removeIcon = document.createElement("div");
+  removeIcon.classList.add("action-icon");
+  removeIcon.dataset.action = "remove";
+  removeIcon.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon icon-remove"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>';
+  iconDiv.appendChild(removeIcon);
 
-    const title = document.createElement("div");
-    title.classList.add("book-title");
-    title.textContent = book.title;
-    bookContainer.appendChild(title);
+  imageDiv.appendChild(iconDiv);
+  bookContainer.appendChild(imageDiv);
 
-    const author = document.createElement("div");
-    author.classList.add("book-author");
-    author.textContent = book.author;
-    bookContainer.appendChild(author);
+  const bookInfo = document.createElement("div");
+  bookInfo.classList.add("flex-col");
+  bookInfo.classList.add("book-info");
 
-    displayGrid.appendChild(bookContainer);
+  const title = document.createElement("div");
+  title.classList.add("book-title");
+  title.textContent = book.title;
+  bookInfo.appendChild(title);
+
+  const author = document.createElement("div");
+  author.classList.add("book-author");
+  author.textContent = book.author;
+  bookInfo.appendChild(author);
+
+  const otherInfo = document.createElement("div");
+  otherInfo.classList.add("flex");
+  otherInfo.classList.add("other-info");
+
+  const pageInfo = document.createElement("p");
+  pageInfo.classList.add("page-info");
+  pageInfo.textContent = `${book.pages} pages`;
+  otherInfo.appendChild(pageInfo);
+
+  const dateInfo = document.createElement("p");
+  dateInfo.textContent = `Added on ${book.dateAdded}`;
+  otherInfo.appendChild(dateInfo);
+  bookInfo.appendChild(otherInfo);
+
+  bookContainer.appendChild(bookInfo);
+  displayGrid.appendChild(bookContainer);
 };
 
 const displayBooks = function () {
-    let bookList = getLibrary();
+  removeChildren(displayGrid);
 
-    bookList.forEach((book) => {
-        displayBook(book);
-    });
+  let bookList = getLibrary();
+
+  bookList.forEach((book) => {
+    displayBook(book);
+  });
 };
 
 addBookBtn.addEventListener("click", () => {
-    const options = document.createDocumentFragment();
+  const options = document.createDocumentFragment();
 
-    GENRES.sort().forEach((genre) => {
-        const option = document.createElement("option");
-        option.value = genre.toLowerCase();
-        option.textContent = genre;
-        options.appendChild(option);
-    });
-    bookGenre.appendChild(options);
+  GENRES.sort().forEach((genre) => {
+    const option = document.createElement("option");
+    option.value = genre.toLowerCase();
+    option.textContent = genre;
+    options.appendChild(option);
+  });
+  bookGenre.appendChild(options);
 
-    modal.showModal();
+  modal.showModal();
 });
 
 const validateForm = function () {
-    if (!bookTitle.value) {
-        bookTitleValidation.textContent = "Please enter a book title";
-        bookTitle.focus();
-        return false;
-    } else bookTitleValidation.textContent = "";
+  if (!bookTitle.value) {
+    bookTitleValidation.textContent = "Please enter a book title";
+    bookTitle.focus();
+    return false;
+  } else bookTitleValidation.textContent = "";
 
-    if (!bookAuthor.value) {
-        bookAuthorValidation.textContent = "Please enter the book author(s)";
-        bookAuthor.focus();
-        return false;
-    } else bookAuthorValidation.textContent = "";
+  if (!bookAuthor.value) {
+    bookAuthorValidation.textContent = "Please enter the book author(s)";
+    bookAuthor.focus();
+    return false;
+  } else bookAuthorValidation.textContent = "";
 
-    if (!bookNumPages.value || bookNumPages.value <= 0) {
-        bookNumPagesValidation.textContent = "Please enter a valid number";
-        bookNumPages.focus();
-        return false;
-    } else bookNumPagesValidation.textContent = "";
+  if (!bookNumPages.value || bookNumPages.value <= 0) {
+    bookNumPagesValidation.textContent = "Please enter a valid number";
+    bookNumPages.focus();
+    return false;
+  } else bookNumPagesValidation.textContent = "";
 
-    return true;
+  return true;
 };
 
 addToLibraryBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (validateForm()) {
-        let imageUrl;
+  event.preventDefault();
+  if (validateForm()) {
+    let imageUrl;
 
-        if (bookCover.files && bookCover.files[0]) {
-            console.log("this goes first.");
-            const fileReader = new FileReader();
-            fileReader.readAsDataURL(bookCover.files[0]);
-            fileReader.addEventListener("load", () => {
-                imageUrl = fileReader.result;
-                console.log(imageUrl);
-            });
-        }
-
-        setTimeout(() => {
-            const newBook = new Book(
-                bookTitle.value,
-                bookAuthor.value,
-                bookDescription.value,
-                bookNumPages.value,
-                bookGenre.value,
-                imageUrl
-            );
-
-            console.log(imageUrl);
-            console.log(newBook);
-            let bookList = getLibrary();
-            bookList.push(newBook);
-            localStorage.setItem("library", JSON.stringify(bookList));
-            displayBook(newBook);
-            modal.close();
-            newBookForm.reset();
-        }, 300);
+    if (bookCover.files && bookCover.files[0]) {
+      console.log("this goes first.");
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(bookCover.files[0]);
+      fileReader.addEventListener("load", () => {
+        imageUrl = fileReader.result;
+        console.log(imageUrl);
+      });
     }
+
+    setTimeout(() => {
+      const newBook = new Book(
+        bookTitle.value,
+        bookAuthor.value,
+        bookDescription.value,
+        bookNumPages.value,
+        bookGenre.value,
+        imageUrl
+      );
+
+      console.log(imageUrl);
+      console.log(newBook);
+      let bookList = getLibrary();
+      bookList.push(newBook);
+      localStorage.setItem("library", JSON.stringify(bookList));
+      displayBook(newBook);
+      modal.close();
+      newBookForm.reset();
+    }, 300);
+  }
 });
 
 displayBooks();
+
+const actionIcons = document.querySelectorAll(".icon");
+
+displayGrid.addEventListener("click", (event) => {
+  target = event.target;
+  if (target.closest(".action-icon")) {
+    let bookId = target.closest(".book-cover").id;
+    let actionType = target.closest(".action-icon").dataset.action;
+    console.log(`${bookId} - ${actionType}`);
+
+    let bookList = getLibrary();
+    const bookIndex = bookList.findIndex(
+      (book) => book.hash === Number(bookId)
+    );
+
+    switch (actionType) {
+      case "read":
+        bookList[bookIndex].isRead = !bookList[bookIndex].isRead;
+        console.log("I finished this book");
+        break;
+
+      case "favorite":
+        bookList[bookIndex].favorite = !bookList[bookIndex].favorite;
+        console.log("I like this book");
+        break;
+    }
+
+    setLibrary(bookList);
+  }
+});
